3034ed0e91fad8ef0e9b384a812acea162641a5c
shiva Ragupathy <ajaiinindia1996@gmail.com>
2022-07-25 16:44:10 +0300

    initial

---
+++ src/module2/activity/operatoractivity.java
@@ -0,0 +1,17 @@
+package module2.activity;
+
+public class operatoractivity
+{
+    public static void main(String[] args) {
+
+        System.out.println("Enter two numbers");
+        int a = 8;
+        int b = 5;
+
+
+
+        //printing the result
+
+        System.out.println("The sum is =" + Integer.sum(a, b));
+    }
+}
