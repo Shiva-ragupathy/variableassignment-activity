1578710bac8a315c523c324d08f62da98050c058
shiva Ragupathy <ajaiinindia1996@gmail.com>
2022-07-25 17:20:46 +0300

    initial

---
+++ src/module2/activity/Findthelargestnumber.java
@@ -0,0 +1,13 @@
+package module2.activity;
+
+public class Findthelargestnumber {
+    public static void main(String[] args) {
+        int i = 8;
+        int j = 10;
+        if (i>j) {
+            System.out.println("i is greater than j");
+        } else {
+            System.out.println ("j is greater than i");
+        }
+    }
+}
