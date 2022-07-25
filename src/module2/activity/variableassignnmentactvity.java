a3254ff369ac8fc3090bcb45deb523c8552923f4
shiva Ragupathy <ajaiinindia1996@gmail.com>
2022-07-25 16:00:31 +0300

    initial

--- src/module2/activity/variableassignnmentactvity.java
+++ src/module2/activity/variableassignnmentactvity.java
@@ -2,12 +2,13 @@ package module2.activity;
 
 public class variableassignnmentactvity {
 
-    public static void main (String args []) {
+    public static void main (String args [])
+    {
         int x = 6;
 
-        x = 6;
+        x = 16;
 
-        system.out.println(x);
+        System.out.println("Result" + x);
 
     }
 }
