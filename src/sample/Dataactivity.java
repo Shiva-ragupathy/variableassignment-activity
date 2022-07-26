a6ae7a9179a5f693a341fe0af42575529220522d
shiva Ragupathy <ajaiinindia1996@gmail.com>
2022-07-26 14:25:19 +0300

    initial

---
+++ src/sample/Dataactivity.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Dataactivity {
+    public static void main(String args[]){
+
+        Date todayDate = new Date();
+        System.out.println(todayDate);
+
+    }
+
+}
+
