f389c1c004422cdf2503fc3ddb02d06acc7f0b01
shiva Ragupathy <ajaiinindia1996@gmail.com>
2022-07-26 09:52:38 +0300

    initial

---
+++ src/module2/activity/Multiplicationtable.java
@@ -0,0 +1,13 @@
+package module2.activity;
+
+public class Multiplicationtable {
+
+        public static void main(String[] args) {
+            for(int i =11; i <=20;i++){
+                for (int j=1; j<=10;j++){
+                    System.out.println(i+" MultiplicationTable");
+                    System.out.println(i + "*" + j + " = "+i*j);
+                }
+            }
+        }
+}
