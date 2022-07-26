b43a47140e72cd8e79ece7c88177bf575edcf166
shiva Ragupathy <ajaiinindia1996@gmail.com>
2022-07-26 14:31:43 +0300

    initial

---
+++ src/sample/Dateactivity.java
@@ -0,0 +1,10 @@
+package sample;
+import java.util.Date;
+
+public class Dateactivity {
+    public static void main(String[] args) {
+        Date todayDate = new Date();
+        System.out.println(todayDate);
+    }
+}
+
