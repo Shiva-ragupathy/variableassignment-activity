79490f3232ef326453be98f76c2242f7da2a0926
shiva Ragupathy <ajaiinindia1996@gmail.com>
2022-07-28 09:09:53 +0300

    initial

---
+++ src/Module6/Animal.java
@@ -0,0 +1,7 @@
+package Module6;
+
+public interface Animal {
+    public void animalSound();
+    public void sleep();
+}
+
---
+++ src/Module6/Cat.java
@@ -0,0 +1,17 @@
+package Module6;
+
+public class Cat implements Animal {
+
+        @Override
+        public void animalSound() {
+                System.out.println("Cat says: 'Meow Meow'!");
+        }
+
+        @Override
+        public void sleep() {
+                System.out.println("Cat is sleeping!");
+        }
+
+}
+
+
